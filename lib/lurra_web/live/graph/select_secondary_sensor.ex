bc7111619c7b88edde31ef83ca0e74267faeb117
gabrielete <gabrielete@gmail.com>
2023-03-01 08:23:00 +0000

    Fix separator in secondary graph wrong

--- lib/lurra_web/live/graph/select_secondary_sensor.ex
+++ lib/lurra_web/live/graph/select_secondary_sensor.ex
@@ -6,18 +6,19 @@ defmodule LurraWeb.Graph.SelectSecondarySensor do
   alias Surface.Components.Form.Submit
   alias Surface.Components.Form.Select
 
+  @separator "/~/@/~/"
+
   prop unit, :string, required: true
 
   def handle_event("save", %{"selector" => [sensor_to_add]}, socket) do
     LurraWeb.Components.Dialog.hide("add-secondary-sensor-dialog")
-    [observer_id, sensor_type] = sensor_to_add
-      |> String.split("-")
+    [observer_id, sensor_type] = String.split(sensor_to_add, @separator)
     send(self(), {:add_sensor, observer_id, sensor_type})
     {:noreply, socket}
   end
 
   defp sensors_options(unit) do
     Monitoring.list_observer_sensors_by_unit(unit)
-    |> Enum.map(fn {_os, observer, sensor} -> {"#{observer.name} - #{sensor.name}", "#{observer.device_id}-#{sensor.sensor_type}"} end)
+    |> Enum.map(fn {_os, observer, sensor} -> {"#{observer.name} - #{sensor.name}", "#{observer.device_id}#{@separator}#{sensor.sensor_type}"} end)
   end
 end
